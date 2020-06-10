04f47b4b79e7b4cc762d8134d3f0151c7fbc2c85
fakhraaslam <fakhraaslam1221@gmail.com>
2020-06-10 23:13:41 +0500

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React ,{useState}from 'react';//shadow dom or virtual dom
 import logo from './logo.svg';
+import {Message} from './Message.js'
 import './App.css';
 
 export default function App() {
-let {count, setcount}=useState(0)
+let {count, setCount}=useState(0)
   return (
     <div >
+      <Message counter={count}/>
    <h3>value of counter is:{count}</h3>
 <button onClick={
   ()=>setCount(count + 1)
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+export function Message(props)
+{
+    return(
+    <h3>value of counter is:{props.count}</h3>
+    );
+}
